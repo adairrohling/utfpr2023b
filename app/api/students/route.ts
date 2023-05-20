3e193782a623a48212f17e115bd69e5a51df27e7
adairrohling <81537071+adairrohling@users.noreply.github.com>
2023-05-20 00:48:11 +0000

    Método POST com algum retorno

--- app/api/students/route.ts
+++ app/api/students/route.ts
@@ -1,10 +1,13 @@
 import { getAllStudent } from "@/app/model/StudentService"
 import { NextResponse } from "next/server"
 
-
 export async function GET(){
     const students = await getAllStudent()
     console.log("Acesssu GET...")
     return NextResponse.json(students)
-  
+}
+
+export async function POST(){
+    console.log("acessou POST")
+    return NextResponse.json({sucess:"ok"})
 }
