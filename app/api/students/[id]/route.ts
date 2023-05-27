dcf74d971343beb18b04f7e996d9b0e7cfebc05c
adairrohling <81537071+adairrohling@users.noreply.github.com>
2023-05-27 00:01:27 +0000

    Padrão Rest OK

--- app/api/students/[id]/route.ts
+++ app/api/students/[id]/route.ts
@@ -1,8 +1,23 @@
 import { NextResponse } from "next/server"
-import {deleteStudent} from "@/app/model/StudentService"
+import { deleteStudent, getStudentById, updateStudentById } from "@/app/model/StudentService"
 
-export async function DELETE(request:Request, {params}:any){
+
+export async function DELETE(request: Request, { params }: any) {
     deleteStudent(params.id)
-    return NextResponse.json({sucess:"ok"})
+    return NextResponse.json({ sucess: "ok" })
 
+}
+export async function GET(request: Request, { params }: any) {
+    const student = await getStudentById(params.id)
+    return NextResponse.json(student);
+}
+export async function PUT(request: Request, { params }: any) {
+    try {
+        const student = await request.json()
+        await updateStudentById(params.id, student.name, student.email)
+        return NextResponse.json(await getStudentById(params.id));
+    } catch (error) {
+        console.log("PUT" + error)
+    }
+    //return await GET(request, params.id as any) 
 }
--- app/model/StudentService.ts
+++ app/model/StudentService.ts
@@ -10,5 +10,12 @@ export async function addStudent(name:string, email:string){
 export async function deleteStudent(id: string){
     await sql`DELETE FROM student WHERE id=${id}`;
 }
+export async function getStudentById(id:string){
+   const {rows:students} = await sql`SELECT * FROM student WHERE id=(${id})`
+   return students[0]
+}
+export async function updateStudentById(id:string, name:string, email:string){
+    await sql`UPDATE student SET name=${name}, email=${email} WHERE id=${id}`
+}
 
 
