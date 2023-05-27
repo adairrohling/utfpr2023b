4817e611ad936930279bdfa4fbe92e4fd84daa78
adairrohling <81537071+adairrohling@users.noreply.github.com>
2023-05-27 20:19:15 +0000

    Change Inicial Page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h2 className='text-center'>Home from Adair</h2>
+    <h2 className='text-center mt-16 bg-red-300'>Home from Adair</h2>
   )
 }
