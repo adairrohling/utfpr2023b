b0276716857453759aee26b38fc66dbc8e06cc83
adairrohling <81537071+adairrohling@users.noreply.github.com>
2023-06-16 19:43:24 +0000

    safe

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,11 +1,19 @@
+import { getServerSession } from 'next-auth/next'
+import { redirect } from 'next/navigation'
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 import Aside from "../components/aside"
 import Nav from "../components/nav"
 
-export default function AdminLayout({
+export default async function AdminLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
+    const session = await getServerSession(authOptions)
+
+  if (!session) {
+    redirect('/api/auth/signin?callbackUrl=/admin')
+  }
     return (
         <div className="md:container md:mx-auto">
             <Nav />
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,16 @@
 
-export default function Admin() {
+import { authOptions } from '../api/auth/[...nextauth]/route'
+
+import { getServerSession } from 'next-auth/next'
+export default async  function Admin() {
+    const session = await getServerSession(authOptions)
     return (
         <div>
-            <h1 className="myclasse">Bem Vindo, Admin</h1>
-        </div>
+        <h1 className='myclass'>Seja Bem Vindo</h1> 
+          <p className='mt-4'>{session?.user?.name}</p>  
+          <p className='mt-4'>{session?.user?.image}</p>  
+         <img src={session?.user?.image as string}  />
+      </div>
 
     )
 }
